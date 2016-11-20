c9d6b86f7f2165ddeb5645eb0f4096f22ce8c2aa
pcatalina <pavlovcatalina@gmail.com>
2016-11-21 00:52:03 +0100

    generate random objects on the landscape

--- Assets/Assets pour Unity/Scripts/RandomObjects.cs
+++ Assets/Assets pour Unity/Scripts/RandomObjects.cs
@@ -1,46 +1,57 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class RandomObjects : MonoBehaviour {
+public class RandomObjects : MonoBehaviour
+{
 
-    public Terrain terrain = Terrain.activeTerrain;
-    public int numberOfObjects = 10; // number of objects to place
-    private int currentObjects; // number of placed objects
-    public GameObject objectToPlace; // GameObject to place
-    private int terrainWidth; // terrain size (x)
-    private int terrainLength; // terrain size (z)
-    private int terrainPosX; // terrain position x
-    private int terrainPosZ; // terrain position z
+    public GameObject paper, plastic, metal, glass;
+    public int numberOfObjects;
+    public float min, max, minr, maxr;
+    public ArrayList arrayList = new ArrayList();
+
     void Start()
     {
-        // terrain size x
-        terrainWidth = (int)terrain.terrainData.size.x;
-        // terrain size z
-        terrainLength = (int)terrain.terrainData.size.z;
-        // terrain x position
-        terrainPosX = (int)terrain.transform.position.x;
-        // terrain z position
-        terrainPosZ = (int)terrain.transform.position.z;
+        PlaceCubes();
     }
-    // Update is called once per frame
-    void Update()
+
+    void PlaceCubes()
     {
-        // generate objects
-        if (currentObjects <= numberOfObjects)
+
+        for (int i = 0; i < numberOfObjects; i++)
         {
-            // generate random x position
-            int posx = Random.Range(terrainPosX, terrainPosX + terrainWidth);
-            // generate random z position
-            int posz = Random.Range(terrainPosZ, terrainPosZ + terrainLength);
-            // get the terrain height at the random position
-            float posy = Terrain.activeTerrain.SampleHeight(new Vector3(posx, 0, posz));
-            // create new gameObject on random position
-            GameObject newObject = (GameObject)Instantiate(GameObject.CreatePrimitive(PrimitiveType.Cube), new Vector3(posx, posy, posz), Quaternion.identity);
-            currentObjects += 1;
+            Instantiate(paper, GeneratedPosition(), Quaternion.Euler(GeneratedRotation()));
+            Instantiate(plastic, GeneratedPosition(), Quaternion.Euler(GeneratedRotation()));
+            Instantiate(metal, GeneratedPosition(), Quaternion.Euler(GeneratedRotation()));
+            Instantiate(glass, GeneratedPosition(), Quaternion.Euler(GeneratedRotation()));
         }
-        if (currentObjects == numberOfObjects)
+    }
+
+    Vector3 GeneratedPosition()
+    {
+        float x, y, z;
+        x = Random.Range(min, max);
+        y = 0.1f;
+        z = Random.Range(min, max);
+
+        Vector3 pos = new Vector3(x, y, z);
+
+        if (arrayList.IndexOf(pos) != -1)
         {
-            Debug.Log("Generate objects complete!");
+            return Vector3.zero;
         }
+        else
+        {
+            arrayList.Add(pos);
+            return pos;
+        }
+    }
+
+    Vector3 GeneratedRotation()
+    {
+        float x, y, z;
+        x = Random.Range(minr, maxr);
+        y = Random.Range(minr, maxr);
+        z = Random.Range(minr, maxr);
+        return new Vector3(x, y, z);
     }
 }
