19a0ded1b11807d7dd4e596df94c98034af5c1c1
pcatalina <pavlovcatalina@gmail.com>
2016-11-21 18:15:05 +0100

    collect script

---
+++ Assets/Assets pour Unity/Scripts/Collect.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collect : MonoBehaviour {
+
+    public int scoreValue = 1;
+
+    void Start()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider intruder)
+    {
+        if (intruder.tag == "Player")
+        {
+            //this.gameObject.SetActive(false);
+            intruder.GetComponent<PlayerBehaviourScript>().AddScore(scoreValue);
+            Destroy(this.gameObject);
+        }
+    }   
+}
